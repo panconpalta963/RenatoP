a878a418c9d90de919f7bbbce12372f41d747762
panconpalta963 <renato.07@gmail.com>
2025-04-16 16:40:44 -0400

    primer commit

---
+++ hola.py
@@ -0,0 +1,30 @@
+# # # nombre="DRenatoylan"
+# # # edad=19
+# # # print("hola", nombre, "su esdad es", edad)
+
+# # # #solicitud de datos
+# # # print("ingrese su nombre")
+# # # nombre=input()
+# # # print("ingrese su edad")
+# # # edad=imput()
+# # # #ejemplo de concetenación
+# # # # print("hola", nombre, "su esdad es", edad)
+
+
+
+# print("ingrese numeros")
+# n1=int(input("nota uno: "))
+# n2=int(input("nota dos: "))
+# n3=int(input("nota tres: "))
+# resutado=(n1+n2+n3)/3
+# print("la suma de los numeros es", resutado)
+
+# if resultado>40:
+#     print("el alumno aprobo")
+# else:
+#     print("el alumno reprobo")
+    
+print("ingrse tres numeros")
+n1=int(input("nota uno: "))
+n2=int(input("nota dos: "))
+n3=int(input("nota tres: "))
